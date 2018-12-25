ba5844684810af53763aaea01672ee68f853ab2b
zheng <15851503917@163.com>
2018-12-25 14:20:09 +0800

    字符串常量作为锁出现的异常

---
+++ src/main/java/com/zjl/synchronizedtest/SynchronizedTest05.java
@@ -0,0 +1,37 @@
+package com.zjl.synchronizedtest;
+/** 
+* 描述：字符串常量作为锁出现的异常
+* @author zhengjinlei 
+* @version 2018年12月25日 下午2:06:54 
+*/
+public class SynchronizedTest05 {
+	public void method(){
+		synchronized ("字符串常量") {//new String("字符串常量")
+			try {
+				while(true){
+					System.out.println("当前线程："+Thread.currentThread().getName()+"开始！");
+					Thread.sleep(1000);
+					System.out.println("当前线程："+Thread.currentThread().getName()+"结束！");
+				}
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+	}
+	public static void main(String[] args) {
+		final SynchronizedTest05 st = new SynchronizedTest05();
+		Thread t1 = new Thread(new Runnable() {
+			public void run() {
+				st.method();
+			}
+		}, "t1");
+		Thread t2 = new Thread(new Runnable() {
+			public void run() {
+				st.method();
+			}
+		}, "t2");
+		t1.start();
+		t2.start();
+	}
+}
+
