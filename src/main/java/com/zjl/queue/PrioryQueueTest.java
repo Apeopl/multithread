773e3553b2879ebd5e10e7ee81d36da266047e7e
zheng <15851503917@163.com>
2018-12-26 14:35:48 +0800

    元素取出时会进行排序

--- src/main/java/com/zjl/queue/PrioryQueueTest.java
+++ src/main/java/com/zjl/queue/PrioryQueueTest.java
@@ -8,18 +8,20 @@ import java.util.concurrent.PriorityBlockingQueue;
 * @version 2018年12月26日 上午11:02:29 
 */
 public class PrioryQueueTest {
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InterruptedException {
 		PriorityBlockingQueue<People> q = new PriorityBlockingQueue<People>();
 		People p1 = new People(12,"zhang");
 		People p2 = new People(26,"li");
 		People p3 = new People(24,"liu");
 		People p4 = new People(16,"niu");
+		People p5 = new People(8,"niu");
 		
 		q.offer(p1);
 		q.offer(p2);
 		q.offer(p3);
 		q.offer(p4);
-		
+		q.offer(p5);
+		q.take();
 		for (People people : q) {
 			System.out.println(people);
 		}
